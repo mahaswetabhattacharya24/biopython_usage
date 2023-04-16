a666818909e6b309e813b7ed79f921e63a8485e8
mahaswetabhattacharya24 <mbhatt17@jh.edu>
2023-04-16 16:45:01 -0400

    Basic DNA String manipulation

--- replication.py
+++ replication.py
@@ -31,6 +31,33 @@ def PatternMatching(Pattern, Genome):
             positions.append(i)
     return positions
 
+def PatternCount(Text, Pattern):
+    positions = [] # output variable
+    c=0
+    for i in range(len(Pattern) - len(Text) + 1):
+        if Pattern[i:i+len(Text)] == Text:
+            c+=1
+    return (c)
+
+def SkewArray(Genome):
+    skew = [0]
+    score = {"A":0, "T":0, "C":-1, "G":1}
+    for i in range(1,len(Genome)+1):
+            skew.append(score[Genome[i-1]] + skew[i-1])
+    return skew
+
+def MinimumSkew(Genome):
+    array = SkewArray(Genome)
+    positions = []
+    count = 0
+    minarray = min(array)
+    for i in array:
+        if i == minarray:
+            positions.append(count)
+        count +=1
+    return positions
+
+
 if __name__ == "__main__":
     sequence = input("Enter the nucleotide sequence: ")
     print("Reversed sequence:\n", Reverse(sequence))
