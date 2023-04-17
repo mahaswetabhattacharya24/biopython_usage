8488aeb2331a99be3caa2713289e4ad253cf83d2
mahaswetabhattacharya24 <mbhatt17@jh.edu>
2023-04-16 22:37:58 -0400

    access enzyme records from kegg and find associated pathways and gene attributes

--- accessing_kegg.py
+++ accessing_kegg.py
@@ -10,3 +10,23 @@ if __name__ == "__main__":
     request = REST.kegg_get("ec:5.4.2.2")
     open("ec_5.4.2.2.txt", "w").write(request.read())
 
+    #Open the downloaded enzyme file
+    records = Enzyme.parse(open("ec_5.4.2.2.txt"))
+    record = list(records)[0]
+    print("Details of the KEGG file\n")
+    print(record.classname)
+
+    #Print the associated pathways
+    print("The associated pathways are:\n")
+    print(record.pathway)
+
+    #Retrieve the gene attributes of the enzyme
+    print("The first 10 gene attributes are:\n")
+    print(record.genes[:10])
+
+    #Print only the gene names
+    print("The gene names are:\n")
+    list_genes = []
+    for x, y in record.genes:
+        list_genes += x.split("\n")
+    print(list_genes[:10])
